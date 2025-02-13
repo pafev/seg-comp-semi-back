fb3adda8eb1f1f5f14dd9c98aa2206e7c180aa93
paulo <paulofvilarimda@gmail.com>
2025-02-13 02:25:33 -0300

    cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app import models
 from app.db import engine
@@ -9,6 +10,19 @@ from app.users import users_routes
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://seg-comp-semi-front-production.up.railway.app",
+    "https://seg-comp-semi-front-production.up.railway.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
